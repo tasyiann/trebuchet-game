751ee658c9c168609d8b76b8a0fa13182b306664
ayiann03@cs.ucy.ac.cy <ayiann03@103ws31.in.cs.ucy.ac.cy>
2018-11-07 10:00:20 +0200

    first commit

---
+++ Assets/LabPhysics/script2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        GetComponent<Rigidbody>().AddForce(new Vector3(0,1,0)*200);
+    }
+}
